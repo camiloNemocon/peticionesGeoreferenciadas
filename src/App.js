978c2b7c4dcab50ef4acf5c1d0a70b67bb08a6a6
camilo.nemocon <c-nemoco@uniandes.edu.co>
2023-01-16 11:30:22 -0500

    se sube el header con cambio en app.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Header from './components/Header'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header/>
         <img src={logo} className="App-logo" alt="logo" />
         ahhhhhhhhhhhhhhhh
-      </header>
+      
     </div>
   );
 }
